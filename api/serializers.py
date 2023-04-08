2474fa4a680655b147f4b410cd662bf4d35678c2
djumanov <diyorbekjumanov1120@gmail.com>
2023-04-08 14:30:44 +0500

    view

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class TaskSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=200)
+    completed = serializers.BooleanField(default=False)
+    description = serializers.CharField()
+    created = serializers.DateTimeField(read_only=True)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+
+class TaskView(APIView):
+    def get(self, request: Request) -> Response:
+        '''get all tasks'''
+        task = Task.objects.first()
+        serializer = TaskSerializer(task)
+        data = serializer.data
+        return Response(data)
+    
