d95aaa50e1d2cc4bd74941b0fb8accea496af1d0
djumanov <diyorbekjumanov1120@gmail.com>
2023-04-08 13:15:15 +0500

    instruction

--- serialization.py
+++ serialization.py
@@ -1,58 +1,30 @@
-# drf-serialization
+from rest_framework import serializers
 
-this is a simple example of how to use drf-serialization. Todo list project without authentication.
-
-## Installation
-
-```bash
-pip install -r requirements.txt
-```
-
-## Serialization object to json
-
-- Task object
-
-```python
 class Task():
     def __init__(self, id, title, description, done):
         self.id          = id
         self.title       = title
         self.description = description
         self.done        = done
-```
 
-- TaskSerializer
-
-```python
-from rest_framework import serializers
 
 class TaskSerializer(serializers.Serializer):
     id          = serializers.IntegerField()
     title       = serializers.CharField()
     description = serializers.CharField()
     done        = serializers.BooleanField()
-```
 
-- TaskSerializer to json
+    def create(self, validated_data):
+        return Task(**validated_data)
 
-```python
-task = Task(1, 'title', 'description', False)
-serializer = TaskSerializer(task)
-serializer.data
-```
 
-## deserialization json to object
-
-```python
 data = {
     'id': 1,
     'title': 'title',
     'description': 'description',
     'done': False
 }
-
 serializer = TaskSerializer(data=data)
 if serializer.is_valid():
     task = serializer.save()
     print(task.title)
-```
